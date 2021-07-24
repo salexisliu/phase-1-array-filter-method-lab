1a782d3759c3fdba3b4c76c446bf0f6a3146d037
Alexis S Liu <syl1350@gmail.com>
2021-07-24 01:24:56 -0400

    commit tree d434a747918f230f9af748ca236b4de0f8879d8a

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+
+// const drivers = ["Betty", 'Bethany', "Beatrice", "Bea", "Bobby", "Charlie"];
+
+
+//  const drivers = [{name: "Betty", hometown: "LA"}, {name: "Sam", hometown: "NY"}, {name: "Blue", hometown: "TX"}]
+
+function findMatching(drivers, driverName) {
+  return drivers.filter((driver) => driver.toLowerCase() === driverName.toLowerCase())
+
+}
+
+function fuzzyMatch(drivers, driverName){
+  return drivers.filter(driver => driver.substr(0, driverName.length) === driverName)
+}                               
+
+// function fuzzyMatch(drivers, driverName){
+//   return drivers.filter(driver => driver.includes(driverName))
+// }                               
+
+
+// console.log(fuzzyMatch(drivers, 'be'))
+// const array = ["Betty", 'Bethany', "Beatrice", "Bea", "Bobby", "Charlie"];
+// console.log(array[0])
+
+function matchName(drivers, driverName){
+  return drivers.filter(driver => driver.name === driverName)
+}
+
